6e1201a7d3cfbd88e98c07385858f633806d76bf
thingsareright <maxthingsareright@gmail.com>
2019-03-06 14:55:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.dou361.ijkplayer.bean.VideoijkBean;
+import com.dou361.ijkplayer.listener.OnShowThumbnailListener;
+import com.dou361.ijkplayer.widget.PlayStateParams;
+import com.dou361.ijkplayer.widget.PlayerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        List<VideoijkBean> list = new ArrayList<>();
+        String url1 = "rtmp://119.23.224.182:1935/live/mystream";
+        String url2 = "rtmp://live.hkstv.hk.lxdns.com/live/hks";
+        VideoijkBean m1 = new VideoijkBean();
+        m1.setStream("标清");
+        m1.setUrl(url1);
+        VideoijkBean m2 = new VideoijkBean();
+        m2.setStream("高清");
+        m2.setUrl(url2);
+        list.add(m1);
+        list.add(m2);
+        View rootView = getLayoutInflater().from(this).inflate(R.layout.activity_main, null);
+        setContentView(rootView);
+        PlayerView playerView = new PlayerView(this,rootView)
+                .setTitle("什么")
+                .setScaleType(PlayStateParams.fitparent)
+                .hideMenu(true)
+                .forbidTouch(false)
+                .showThumbnail(new OnShowThumbnailListener() {
+                    @Override
+                    public void onShowThumbnail(ImageView ivThumbnail) {
+
+                    }
+                })
+                .setPlaySource(list)
+                .startPlay();
+    }
+}
